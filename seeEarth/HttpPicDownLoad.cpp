d6cd06e6f0d0538d10d9a7e1113e67db05c6c150
TiriSane <sherlzerock@icloud.com>
2016-10-06 13:14:35 +0800

    Update HttpPicDownLoad.cpp

--- seeEarth/HttpPicDownLoad.cpp
+++ seeEarth/HttpPicDownLoad.cpp
@@ -3,7 +3,36 @@
 
 using namespace std;
 
-bool downPicFromURL(string url)
+bool downloadPics(vector<string> urls)
+{
+	string p1 = urls[0];
+	string p2 = urls[1];
+	string p3 = urls[2];
+	string p4 = urls[3];
+
+	bool flag = true;
+
+	if (!downPicFromURL(p1,"C:\\1.png"))
+	{
+		flag = false;
+	}
+	if (!downPicFromURL(p2, "C:\\2.png"))
+	{
+		flag = false;
+	}
+	if (!downPicFromURL(p3, "C:\\3.png"))
+	{
+		flag = false;
+	}
+	if (!downPicFromURL(p4, "C:\\4.png"))
+	{
+		flag = false;
+	}
+
+	return flag;
+}
+
+bool downPicFromURL(const string &url,const string &saveName)
 {
 	bool flag = true;
 	CString URL = url.c_str();
@@ -13,7 +42,7 @@ bool downPicFromURL(string url)
 	CStdioFile imgFile;
 	char buff[BUFFSIZE];                              // 缓存  
 
-	imgFile.Open("C:\\earth.png", CFile::modeCreate | CFile::modeWrite | CFile::typeBinary);
+	imgFile.Open(saveName.c_str(), CFile::modeCreate | CFile::modeWrite | CFile::typeBinary);
 
 	DWORD dwStatusCode;
 	httpFile->QueryInfoStatusCode(dwStatusCode);

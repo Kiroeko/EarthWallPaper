19adc222afbcc2d0b659befa7a25e61dcf620648
TiriSane <sherlzerock@icloud.com>
2016-10-06 13:13:31 +0800

    Update setWallPaper.h

--- seeEarth/setWallPaper.h
+++ seeEarth/setWallPaper.h
@@ -1,9 +1,14 @@
 #ifndef _SET_WALL_PAPER_H_
 #define _SET_WALL_PAPER_H_
 
-#include <windows.h>  
+#include <string>
+#include <cstdlib>
+#include <afxwin.h>
 #include <atlimage.h>
 
 void SetWallPaper();
+void MixPics();
+void UpDowmMix(const std::string &A, const std::string &B, const std::string &C);
+void LRMix(const std::string &A, const std::string &B);
 
 #endif
